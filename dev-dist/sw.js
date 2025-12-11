a19d073feb7614855fe4b190ca1293889409619a
ccwq <scwqd@qq.com>
2025-12-11 17:55:55 +0800

    feat(图片处理): 添加多图输入预览与自动下载功能
    支持文件选择与拖拽同时输入多张图片，形成可遍历的待处理队列
    预览区域同步展示多张图片及其裁切预览
    新增自动下载复选框，默认关闭且状态持久化

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.0dno7u662hs"
+    "revision": "0.ep59rvjehm4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
