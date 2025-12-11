cbc479f0b041b0e3f621afdfdffe44a1763f3f21
ccwq <scwqd@qq.com>
2025-12-11 10:50:51 +0800

    chore:

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.e2i19nh8qd"
+    "revision": "0.r8gkg6t7t48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
