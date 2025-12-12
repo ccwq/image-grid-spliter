67b9d577754410bb37194bc060745c52ec7eb09d
ccwq <scwqd@qq.com>
2025-12-12 15:17:24 +0800

    chore: 更新版本号至1.0.3并更新sw.js的revision值

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5119daf5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ep59rvjehm4"
+    "revision": "0.2sua2puto9k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
